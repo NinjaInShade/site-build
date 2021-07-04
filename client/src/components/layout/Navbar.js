f274a4f118c5e09aae533157f20a0321e385c280
Leon Michalak <leonmichalak6@gmail.com>
2021-07-04 18:02:06 +0100

    navbar properties extracted into css vars

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,29 +1,29 @@
 // Libraries
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 // CSS
-import "../../css/Navbar.css";
+import '../../css/Navbar.css';
 
 // Components
-import Button from "../general/Button";
+import Button from '../general/Button';
 
 // Images
-import BrandLogo from "../../resources/BrandLogo.svg";
+import BrandLogo from '../../resources/BrandLogo.svg';
 
 export default function Navbar() {
   return (
     <nav>
       <div>
-        <img src={BrandLogo} alt="Brand logo" />
-        <h1>Site Builder</h1>
+        <img src={BrandLogo} alt='Brand logo' />
+        <strong>Site Builder</strong>
       </div>
       <div>
         <Button>
-          <a href="#beginning">Start the proccess</a>
+          <a href='#beginning'>Start the proccess</a>
         </Button>
-        <Link to="/Authenticate" className="login-btn">
-          <i className="fas fa-sign-in-alt"></i>
+        <Link to='/Authenticate' className='login-btn'>
+          <i className='fas fa-sign-in-alt'></i>
         </Link>
       </div>
     </nav>
