d8400ece9c58a29c2df920ef9b15055a56b8a3d9
Leon Michalak <leonmichalak6@gmail.com>
2021-07-19 23:00:39 +0100

    added tood

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import './App.css';
 // TODO: Make navbar responsive with a sidebar menu
 // TODO: Make dropdown absolute so window height doesn't change.
 // TODO: Redesign control panel
+// TODO: Implement authentication
 
 // TODO: Code control panel - sidebar component & project card component
 // TODO: control panel should load sites data into state on component mount ( useEffect hook )
